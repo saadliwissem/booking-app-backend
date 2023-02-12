acdc76f4a78b0c086b312996464059f22d121be7
wissem <saadliwissem88@gmail.com>
2023-02-12 10:18:05 +0100

    fixing bugs

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const app = express();
 dotenv.config();
 //config cross origin
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(Access-Control-Allow-Origin, "*");
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    Access-Control-Allow-Headers,
+    Origin, X-Requested-With, Content-Type, Accept
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    Access-Control-Allow-Methods,
+    GET, POST, PATCH, DELETE, OPTIONS
   );
   next();
 });
